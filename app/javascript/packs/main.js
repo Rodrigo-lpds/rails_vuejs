ce2265d53df29a017b7254eda1c84be4e18789c5
Rodrigo Lucas <rodrigo.lpdsilva@gmail.com>
2021-09-04 16:59:18 -0300

    add table component

--- app/javascript/packs/main.js
+++ app/javascript/packs/main.js
@@ -14,13 +14,13 @@ Vue.use(Vuetify)
 
 document.addEventListener('DOMContentLoaded', () => {
   const app = new Vue({
+    vuetify: new Vuetify(),
     render: h => h(App)
   }).$mount()
   document.body.appendChild(app.$el)
 
   console.log(app)
 })
-
 
 // The above code uses Vue without the compiler, which means you cannot
 // use Vue to target elements in your existing html templates. You would
